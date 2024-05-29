2e167f188ea6e7db3636c4c1637d9dfa763a5ab7
hsayvaidya23 <bobbynaldocr7@gmail.com>
2024-05-29 19:27:23 +0530

    fix/jobs

--- public/assets/jobs/index.js
+++ public/assets/jobs/index.js
@@ -1,221 +1,116 @@
-import acc from './acc.png';
-import aim from './aim.png';
-import bsh_walls_floors from './bsh_walls_floors.png';
-import dct from './dct.png';
-import diligenceVault from './diligenceVault.png';
-import educohire from './educohire.png';
-import entiovi from './entiovi.png';
-import gray from './gray.png';
-import matrix from './matrix.png';
-import maxgen from './maxgen.png';
-import no_logo from './no_logo.png';
-import quantasis from './quantasis.png';
-import sourced from './sourced.png';
-import spherex from './spherex.png';
-import true_data from './true_data.png';
-import uplers from './uplers.png';
-import yojito from './yojito.png';
-import sahir_web_solutions_logo from './sahir_web_solutions_logo.jpg';
-import walmartglobaltechindia_logo from './walmartglobaltechindia_logo.jpg';
-import mitchell_martin_logo from './mitchell_martin_logo.jpg';
-import weareuplers_logo from './weareuplers_logo.jpg';
-import xemplarinsights_logo from './xemplarinsights_logo.jpg';
-import epam_systems_logo from './epam_systems_logo.jpg';
-import tata_consultancy_services_logo from './tata_consultancy_services_logo.jpg';
-import clearspot_ai_logo from './clearspot_ai_logo.jpg';
-import mankaratechnologies_logo from './mankaratechnologies_logo.jpg';
-import workout_fitness_group_logo from './workout_fitness_group_logo.jpg';
-import cognitivecloud from './cognitivecloud.jpg';
-import apexdigitalvc_logo from './apexdigitalvc_logo.jpg';
-import virtusa_logo from './virtusa_logo.jpg';
-import holcim_logo from './holcim_logo.jpg';
-import aws from './aws.png';
-import searce from './searce.svg';
-import pivotree from './pivotree.png';
-import instaciti from './instaciti.png';
-import ey from './ey.png';
-import virtusa from './virtusa.png';
-import ericsson from './ericsson.png';
-import cgi from './cgi.png';
-import teglobals from './teglobals.png';
-import otherside from './otherside.svg';
-import cloud9 from './cloud9.jpeg';
-import cloudseed from './cloudseed.png';
-import impetus from './impetus.svg';
-import logonews from './logonews.png';
-import skynet from './skynet.jpeg';
-import bigspire_logo from './bigspire_logo.gif';
-import think from './think.png';
-import galgali from './galgali.jpg';
-import skyway from './skyway.png';
-import oceonic from './oceonic.jpeg';
-import koshali from './koshali.jpeg';
-import systos from './systos.jpeg';
-import volts from './volts.jpeg';
-import ace from './ace.jpg';
-import globus from './globus.jpeg';
-import Gururo from './Gururo.png';
-import explorogent from './explorogent.jpg';
-import fisheye from './fisheye.jpg';
-import indeed from './indeed.jpg';
-import neo from './neo.png';
-import iide from './iide.png';
-import zf from './zf.jpg';
-import manitenserv from './manitenserv.jpeg';
-import ansrsource from './ansrsource.png';
-import gianteagle from './gianteagle.png';
-import interniq from './interniq.png';
-import ispapp from './ispapp.png';
-import freightcom from './freightcom.jpg';
-import pathfactory from './pathfactory.png';
-import altx from './altx.png';
-import AXS from './AXS.png';
-import Aricent from './Aricent.png';
-import only_much_louder_logo from './only_much_louder_logo.jpeg';
-import Baker_Hughes from './Baker Hughes.png';
-import Media_Guru from './Media Guru.png';
-import AABASOFT from './AABASOFT.png';
-import Flywings_enterprises from './Flywings enterprises.png';
-import Thingularity_new from './Thingularity_new.svg';
-import MATRIKA_LOGO_R from './MATRIKA-LOGO-R.jpeg';
-import Mirabilis_design_incorporation from './Mirabilis Design Incorporation.png'
-import Techno_Growth_logo from './Techno-Growth-logo.svg';
-import Moodys_analytics from './Moodys Analytics.png';
-import NLINE_LOGO from './NLINE-LOGO.png';
-import Xponent_N from './Xponent N.png';
-import finacplus_logo from './finacplus-logo.png';
-import SUN_Mobility from './SUN Mobility.png';
-import corp_id from './corp_id.png';
-import MIT_Logo from './MIT_Logo.jpg'
-import akkodis from './akkodis.png';
-import AS_Global from './AS Global.png';
-import Acceron from './Acceron.png';
-import hirist_tech_logo from './hirist-tech-logo.webp';
-import hitachi_logo from './hitachi-logo.svg';
-import Malabar_group from './Malabar Group.png';
-import Confidential from './Confidential.jpeg';
-import Makerble from './Makerble.png';
-import lte from './lte.png';
-import praella from './praella.png';
-import dtcc from './dtcc.png';
-import meera from './meera.png';
-import path from './path.png';
-import del from './del.png'
-
-import connect4 from './connect4.png';
-import gala from './gala.jpeg';
-import iotics from './iotics.jpeg';
-import startwith from './startwith.jpeg';
-import tekkr from './tekkr.jpeg';
+import simplia from "./simplia.png";
+import wasp from "./wasp.png";
+import tventurer from "./tventurer.png";
+import ding from "./ding.png";
+import weinsoft from "./weinsoft.png";
+import microtech from "./microtech.png";
+import devlov from "./devlov.png";
+import bestiro from "./bestiro.png";
+import probooking from "./probooking.png";
+import avalanche from "./avalanche.png";
+import spider from "./spider.png";
+import maig from "./maig.png";
+import statfinity from "./statfinity.png";
+import pairsoft from "./pairsoft.png";
+import crescer from "./crescer.png";
+import vikara from "./vikara.png";
+import telus from "./telus.png";
+import simwell from "./simplia.png";
+import afaq from "./afaq.png";
+import EnactOn from "./EnactOn.png";
+import Aristocrat from "./Aristocrat.png";
+import Viacom18 from "./Viacom18.png";
+// import Infosys from "./Infosys.png";
+import Dimagi from "./Dimagi.png";
+import overt from "./overt.png";
+import Travitons from "./Travitons .png";
+// import BigSpire from "./BigSpire .png";
+import Opacify from "./Opacify.png";
+import Parkqwik from "./Parkqwik .png";
+import enaction from "./enacton.jpeg";
+import BigSpire from "./BigSpire .png";
+// import overt from "./overt.png";
+import Dieselship from "./Dieselship.jpeg";
+import Travitions from "./Travitons .png";
+import Webfymedia from "./Webfymedia.jpeg";
+import Hindustan from "./Hinduatan.jpeg";
+import Webpariwar from "./Webpariwar.jpeg";
+import Dfavo from "./Dfavo.jpeg";
+import Sunovatech from "./Sunovatech.png";
+import Clarivate from "./Clarivate.jpeg";
+import Micron from "./Micron.jpeg";
+import Infosys from "./Infosys.png";
+import Elsner from "./Elsner.jpeg";
+import BOEING from "./BOEING.jpeg";
+import PurpleDrive from "./PurpleDrive.jpeg";
+import ComUnus from "./ComUnus.jpeg";
+import Irdeto from "./Irdeto.jpeg";
+import NASDAQ from "./NASDAQ.jpeg";
+import TYA from "./TYA.jpeg";
+import workmates from './workmates.png'
+import cloud9 from './cloud9.png'
+import stactec from './stantec.png'
+import techbrein from './techbrein.png'
+import rapydar from './rapydar.png'
+import ValueMentor from './ValueMentor.png'
+import CloudJournee from './CloudJournee.png'
+import Vnnogile from './Vnnogile.png'
+import enacton from './enacton.jpeg'
 
 export {
-    acc,
-    aim,
-    bsh_walls_floors,
-    dct,
-    diligenceVault,
-    educohire,
-    entiovi,
-    gray,
-    matrix,
-    maxgen,
-    no_logo,
-    quantasis,
-    sourced,
-    spherex,
-    true_data,
-    uplers,
-    yojito,
-    sahir_web_solutions_logo,
-    walmartglobaltechindia_logo,
-    mitchell_martin_logo,
-    weareuplers_logo,
-    xemplarinsights_logo,
-    epam_systems_logo,
-    tata_consultancy_services_logo,
-    clearspot_ai_logo,
-    mankaratechnologies_logo,
-    workout_fitness_group_logo,
-    cognitivecloud,
-    apexdigitalvc_logo,
-    virtusa_logo,
-    holcim_logo,
-    aws,
-    searce,
-    pivotree,
-    instaciti,
-    ey,
-    virtusa,
-    ericsson,
-    cgi,
-    teglobals,
-    otherside,
-    cloud9,
-    cloudseed,
-    impetus,
-    logonews,
-    skynet,
-    bigspire_logo,
-    think,
-    galgali,
-    skyway,
-    oceonic,
-    koshali,
-    systos,
-    volts,
-    ace,
-    globus,
-    explorogent,
-    fisheye,
-    indeed,
-    neo,
-    iide,
-    zf,
-    manitenserv,
-    ansrsource,
-    gianteagle,
-    interniq,
-    ispapp,
-    freightcom,
-    pathfactory,
-    Gururo,
-    altx,
-    AXS,
-    Aricent,
-    only_much_louder_logo,
-    Baker_Hughes,
-    Media_Guru,
-    AABASOFT,
-    Flywings_enterprises,
-    Thingularity_new,
-    MATRIKA_LOGO_R,
-    Mirabilis_design_incorporation,
-    Techno_Growth_logo,
-    Moodys_analytics,
-    NLINE_LOGO,
-    Xponent_N,
-    finacplus_logo,
-    SUN_Mobility,
-    corp_id,
-    MIT_Logo,
-    akkodis,
-    AS_Global,
-    Acceron,
-    hirist_tech_logo,
-    hitachi_logo,
-    Malabar_group,
-    Confidential,
-    Makerble,
-    lte,
-    praella,
-    dtcc,
-    meera,
-    path,
-    del,
-
-    connect4,
-    gala,
-    iotics,
-    startwith,
-    tekkr,
+  simplia,
+  CloudJournee,
+  Vnnogile,
+  enacton,
+  ValueMentor,
+  rapydar,
+  techbrein,
+  stactec,
+  wasp,
+  tventurer,
+  ding,
+  weinsoft,
+  microtech,
+  devlov,
+  bestiro,
+  probooking,
+  avalanche,
+  spider,
+  maig,
+  cloud9,
+  statfinity,
+  pairsoft,
+  crescer,
+  vikara,
+  telus,
+  simwell,
+  afaq,
+  EnactOn,
+  Aristocrat,
+  Viacom18,
+  Infosys,
+  Dimagi,
+  overt,
+  Travitons,
+  BigSpire,
+  Opacify,
+  Parkqwik,
+  enaction,
+  Dieselship,
+  Travitions,
+  Webfymedia,
+  Hindustan,
+  Webpariwar,
+  Dfavo,
+  Sunovatech,
+  Clarivate,
+  Micron,
+  Elsner,
+  BOEING,
+  PurpleDrive,
+  ComUnus,
+  Irdeto,
+  NASDAQ,
+  TYA,
+  workmates
 };
